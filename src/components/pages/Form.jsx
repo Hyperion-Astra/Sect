9d751d4674b38f3478223cfd4c18907ea070b783
hyperion-astra <eugeneemovon@gmail.com>
2024-11-07 01:38:19 +0100

    rt

--- src/components/pages/Form.jsx
+++ src/components/pages/Form.jsx
@@ -19,6 +19,7 @@ const ServiceApplication = () => {
 
   return (
     <div style={containerStyle}>
+      <br /><br /><br />
       <h2 style={titleStyle}>Apply for Our Security Services</h2>
       <form
         action="https://formspree.io/f/xdkogzrk" // Replace with your Formspree URL
