9f2568fc9298ea52910ff97952a482bf384788c1
hyperion-astra <eugeneemovon@gmail.com>
2024-11-08 23:17:22 +0100

    et

--- src/components/pages/Portfolio.jsx
+++ src/components/pages/Portfolio.jsx
@@ -21,7 +21,7 @@ const Portfolio = () => {
                     <img src="./ugwumba.jpg" alt="Dr. Diala Callistus Ugwumba" className="about-image"
                 style={{ width: "150px", height: "auto", marginRight: "20px" }} />
                     <p>
-                        Dr. Ugwumba holds a doctorate degree in Management (Corporate Finance) from Walden University, a Masters in Law (LLM) from Northeastern University, and an LLB Law degree from the National Open University of Nigeria. He also holds an MBA in Finance and a degree in Actuarial Science from the University of Lagos.
+                        Dr. Ugwumba holds a doctorate degree in Management (Corporate Finance) from Walden University, a Masters in Law (LLM) from Northeastern University, a B.Sc and an LLB Law degree from the National Open University of Nigeria. He also holds an MBA in Finance and a degree in Actuarial Science from the University of Lagos.
                     </p>
                     <p>
                         Dr. Ugwumba oversees several companies in the United States and Nigeria, offering services ranging from financial consultancy to legal support and security services.
@@ -34,19 +34,20 @@ const Portfolio = () => {
                     <div className="company-list">
                         <div className="company-item">
                             <h4>Mbacal Group LLC</h4>
-                            <p>30 N Gould St #34769, Sheridan WY 82801</p>
+                            <p>1712 Pioneer Ave, Suite 101 WY 82001</p>
                             <p>Tel: 1(209)-822-2194</p>
                         </div>
                         <div className="company-item">
-                            <h4>Integrated Financial & Legal Consult LIC 0106887</h4>
-                            <p>1680 S. E Street, Suite B- 49</p>
+                            <h4>Integrated Financial & Legal Consult LIC 0106887 & 2447853</h4>
+                            <p>1680 South East Street, Suite 49. San Bernardino CA. 92408</p>
                             <p>Tel: 760-953-4853</p>
+                            <p>Email: integratedflc@gmail.com</p>
                         </div>
                         <div className="company-item">
                             <h4>Mbacal Auto & Dismantle LIC 06435</h4>
                         </div>
                         <div className="company-item">
-                            <h4>Mbacal Securities Services LIC 2648619</h4>
+                            <h4>Mbacal Security and Investigations LIC 122308</h4>
                             <p>Suite B49, 1680 South ‘E’ Street, San Bernardino CA. 92408</p>
                             <p>Tel: 01-909-756-3139 / 01-760-953-4853</p>
                         </div>
