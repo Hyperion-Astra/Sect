2e0d4cb7305c289f3ddac9d0600205ce62362d77
hyperion-astra <eugeneemovon@gmail.com>
2024-11-25 23:37:20 +0100

    mmm

--- src/components/pages/Foltlab.jsx
+++ src/components/pages/Foltlab.jsx
@@ -20,21 +20,22 @@ const Foltlab = () => {
                     <h2>Get In Touch</h2>
                     <p>We'd love to hear from you. Get in touch with us for inquiries, partnerships, or any other questions.</p>
 
-                    <form>
-                        <div className="form-group">
-                            <label htmlFor="name">Your Name</label>
-                            <input type="text" id="name" placeholder="Enter your name" />
-                        </div>
-                        <div className="form-group">
-                            <label htmlFor="email">Your Email</label>
-                            <input type="email" id="email" placeholder="Enter your email" />
-                        </div>
-                        <div className="form-group">
-                            <label htmlFor="message">Your Message</label>
-                            <textarea id="message" placeholder="Write your message"></textarea>
-                        </div>
-                        <button type="submit" >Send Message</button>
-                    </form>
+        <form action="https://formspree.io/f/movqnozq" method="POST"> 
+    <div className="form-group">
+    <label htmlFor="name">Your Name</label>
+    <input type="text" id="name" name="name" placeholder="Enter your name" required />
+    </div>
+    <div className="form-group">
+    <label htmlFor="email">Your Email</label>
+    <input type="email" id="email" name="email" placeholder="Enter your email" required />
+    </div>
+    <div className="form-group">
+    <label htmlFor="message">Your Message</label>
+    <textarea id="message" name="message" placeholder="Write your message" required></textarea>
+    </div>
+    <button type="submit">Send Message</button>
+</form>
+
                 </div>
 
                 {/* Contact Information */}
